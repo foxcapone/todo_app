819f7a5ad64ff0a5deb90ebf2870998fa28b0ff4
Jeffrey Woodard <icikalu@gmail.com>
2017-03-06 10:33:22 -0600

    Setup update and destroy to use Javascript and AJAX

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -1,5 +1,5 @@
 class UserTasksController < ApplicationController
-  before_action :all_tasks, only: [:index, :create]
+  before_action :all_tasks, only: [:index, :create, :update, :destroy]
   before_action :set_user_task, only: [:show, :edit, :update, :destroy]
 
   # GET /user_tasks
@@ -44,10 +44,12 @@ class UserTasksController < ApplicationController
   def update
     respond_to do |format|
       if @user_task.update(user_task_params)
-        format.html { redirect_to @user_task, notice: 'User task was successfully updated.' }
+        format.html { redirect_to @user_task }
+        format.js
         format.json { render :show, status: :ok, location: @user_task }
       else
         format.html { render :edit }
+        format.js { render :edit }
         format.json { render json: @user_task.errors, status: :unprocessable_entity }
       end
     end
@@ -58,7 +60,8 @@ class UserTasksController < ApplicationController
   def destroy
     @user_task.destroy
     respond_to do |format|
-      format.html { redirect_to user_tasks_url, notice: 'User task was successfully destroyed.' }
+      format.html { redirect_to user_tasks_url }
+      format.js
       format.json { head :no_content }
     end
   end
